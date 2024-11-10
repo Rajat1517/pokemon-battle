3c5fc48b32a072a74f4adf599ab92394ccfc9600
Rajat Mishra <rajatmishra0815@gmail.com>
2024-11-10 18:46:04 +0530

    Implemented Configure feature

---
+++ src/constants/pokemon.js
@@ -0,0 +1,6 @@
+export const MOVES= {
+    "pikachu":["Mega-Punch","Thunderbolt","Quick-Attack","Iron-Tail","Body-Slam","Takedown"],
+    "bulbasaur": ["Razor-leaf","Solar-Beam","Vine-Whip","Tackle","Bind","Mud-Slap"],
+    "charmander": ["Emmber","Flamethrower","Scratch","Dragon-Rage","Skull-Bash","Seismic-Toss"],
+    "squirtle": ["Blizzard","Mist","Body-Slam","Hydro-pump","Bubble","Ice-Beam"],
+}
