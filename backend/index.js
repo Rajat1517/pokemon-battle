8e820a81408b798c906d84bd276853038fc4055e
Rajat Mishra <rajatmishra0815@gmail.com>
2024-11-25 23:03:45 +0530

    Implemented socket transaction for Attack

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const app = express();
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const expressServer = createServer(app);
+const io = new Server(expressServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
-app.listen(5000, () => {
+
+io.on("connection", (socket) => {
+  socket.on("attackForServer", (attack) => {
+    console.log("Attacked", attack);
+    socket.emit("attackForClient", {
+      player: 1,
+      delta: 20,
+    });
+  });
+
+
+});
+
+expressServer.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
--- frontend/src/screens/Battle.jsx
+++ frontend/src/screens/Battle.jsx
@@ -1,20 +1,45 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { UtilContext } from "../contexts/UtilContext";
+import { io } from "socket.io-client";
 
 function Battle() {
   const { pokemon, character, moves } = useContext(UtilContext);
   const [health1, setHealth1] = useState(100);
   const [health2, setHealth2] = useState(100);
 
-  const decreaseHealth = (player, delta) => {
-    delta = parseInt(window.prompt("Enter the change"));
-    player = parseInt(window.prompt("Enter player"));
+  const socket = io("http://localhost:5000/");
+
+  useEffect(() => {
+    console.log("on");
+    socket.on("attackForClient", (attack) => {
+      const { player, delta } = attack;
+      // console.log("Listened Attack result", player,delta);
+
+      decreaseHealth(player, delta);
+    });
+
+    return () => {
+      console.log("off");
+      socket.off("attackForClient");
+    };
+  }, []);
+
+  const attack = () => {
+    console.log("Emitting attack");
+    socket.emit("attackForServer", "i am attacking");
+  };
+
+  const decreaseHealth = (player=1, delta=20) => {
+      // delta = parseInt(window.prompt("Enter the change"));
+      console.log("Listened Attack result", player,delta);
+      // player = parseInt(window.prompt("Enter player"));
     delta = Math.min(player === 1 ? health1 : health2, delta);
     const p = document.getElementById(player === 1 ? "player1" : "player2");
     let id = null,
       x = 0,
       time = ~~(500 / delta);
     clearInterval(id);
+    // p.style.width=`${(player === 1 ? health1 : health2) - delta}%`;
     id = setInterval(() => {
       let width = p.style.width;
       width = width.slice(0, -1);
@@ -24,10 +49,11 @@ function Battle() {
         p.style.backgroundColor = "orange";
       }
       if (x === delta + 1) {
-        player === 1
-          ? setHealth1((prev) => prev - delta)
-          : setHealth2((prev) => prev - delta);
+        // player === 1
+        //   ? setHealth1((prev) => prev - delta)
+        //   : setHealth2((prev) => prev - delta);
         clearInterval(id);
+        console.log("done");
       } else {
         p.style.width = `${(player === 1 ? health1 : health2) - x}%`;
         x++;
@@ -118,15 +144,15 @@ function Battle() {
             <button className="attack" onClick={decreaseHealth}>
               {moves[0]}
             </button>
-            <button className="attack" onClick={decreaseHealth}>
+            <button className="attack" onClick={attack}>
               {moves[1]}
             </button>
           </div>
           <div>
-            <button className="attack" onClick={decreaseHealth}>
+            <button className="attack" onClick={attack}>
               {moves[2]}
             </button>
-            <button className="attack" onClick={decreaseHealth}>
+            <button className="attack" onClick={attack}>
               {moves[3]}
             </button>
           </div>
